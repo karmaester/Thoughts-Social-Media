63e924b09282865fddd3395ff657c104259df98e
Khristian Rojas <khristianrojas15@gmail.com>
2021-01-14 13:30:29 -0300

    Install app gems

---
+++ app/models/thought.rb
@@ -0,0 +1,2 @@
+class Thought < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :thoughts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'thoughts#index'
 end
