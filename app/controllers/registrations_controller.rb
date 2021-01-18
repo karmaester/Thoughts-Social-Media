4d4d6e984c605e084106ec36e4601652be22f5fb
Khristian Rojas <khristianrojas15@gmail.com>
2021-01-18 11:52:33 -0300

    Add params for images

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
     private_methods
 
     def sign_up_params
-        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :avatar_image, :cover_image)
     end
 
     def acount_update_params
-        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password, :avatar_image, :cover_image)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  has_one_attached :avatar_image
+  has_one_attached :cover_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def any_attached?
+    ActiveStorage::Attachment.where(record_type: model_name.to_s, record_id: id).any?
+  end
 end
