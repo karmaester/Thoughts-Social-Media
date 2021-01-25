27709ac31a561d580e9999e649eca11da94405bf
Khristian Rojas <khristianrojas15@gmail.com>
2021-01-25 14:08:58 -0300

    Add counters and display image helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,27 @@
 module UsersHelper
-    def follow_unfollow(user)
-        cntnt = link_to 'Unfollow', follow_path(user), method: :delete, class: 'btn'
-        return cntnt if current_user.followds.include?(user)
-    
-        cntnt = link_to 'Follow', follows_path(follow: { follower_id: current_user.id, followed_id: user.id}), method: :post, class: 'btn'
-        return cntnt unless current_user.followds.include?(user) || current_user == user
-    
-        nil
-      end
+  def follow_unfollow(user)
+      cntnt = link_to 'Unfollow', follow_path(user), method: :delete, class: 'button is-info'
+      return cntnt if current_user.followds.include?(user)
+  
+      cntnt = link_to 'Follow', follows_path(follow: { follower_id: current_user.id, followed_id: user.id}), method: :post, class: 'button is-info'
+      return cntnt unless current_user.followds.include?(user) || current_user == user
+  
+      nil
+  end
+
+  def user_avatar(user)
+    if user.avatar_image.attached?
+      image_tag user.avatar_image
+    else
+      image_tag('avatar.jpeg', class: 'rounded-circle', height: 130, width: 130)
+    end
+  end
+
+  def user_cover
+    if @user.cover_image.attached?
+      image_tag @user.cover_image, class: 'cover'
+    else
+      image_tag('cover.jpg', class: 'cover')
+    end
+  end
 end
