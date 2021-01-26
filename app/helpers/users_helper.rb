7287fb5261b373deffa1693f86dfcc9210666ba3
Khristian Rojas <khristianrojas15@gmail.com>
2021-01-26 15:41:04 -0300

    Add display_unfollowed helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,15 @@
 module UsersHelper
   def follow_unfollow(user)
-    cntnt = link_to 'Unfollow', follow_path(user), method: :delete, class: 'button is-info'
-    return cntnt if current_user.followds.include?(user)
-
-    cntnt = link_to 'Follow', follows_path(follow: { follower_id: current_user.id, followed_id: user.id }),
-                    method: :post, class: 'button is-info'
-    return cntnt unless current_user.followds.include?(user) || current_user == user
-
-    nil
+    if current_user == user
+      nil
+    elsif current_user.followds.include?(user)
+      cntnt = link_to 'Unfollow', follow_path(user), method: :delete, class: 'button is-info'
+      return cntnt 
+    else
+      cntnt = link_to 'Follow', follows_path(follow: { follower_id: current_user.id, followed_id: user.id }),
+                      method: :post, class: 'button is-info'
+      return cntnt 
+    end
   end
 
   def user_avatar(user)
@@ -26,6 +28,14 @@ module UsersHelper
       image_tag(
         'https://i.ibb.co/ThDZL98/Chromatic-Linked-In-Cover-Photo-Background.png', class: 'cover'
       )
+    end
+  end
+
+  def display_unfollowed(user)
+    if user != current_user && !current_user.followds.include?(user)
+      true
+    else
+      false
     end
   end
 end
